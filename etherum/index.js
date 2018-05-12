d268f24b1bb91d0ef11ec5a91b4c1b2a66548e9a
jimmyadg <jimmyadg@gmail.com>
2018-05-12 15:05:18 -0400

    update

--- etherum/index.js
+++ etherum/index.js
@@ -1,7 +1,7 @@
 const HDWalletProvider = require('truffle-hdwallet-provider'),
   Web3 = require('web3'),
   fs = require('fs-extra'),
-  compileERC721Interface = require('./build/ERC721.json'),
+  compileKittyCore = require('./build/KittyCore.json'),
   provider = new HDWalletProvider(
     'weekend that universe snack have alter silk wheat razor ladder seminar link',
     'https://mainnet.infura.io/D3SAVJhqB6IuHvI8DAeA'
@@ -15,7 +15,7 @@ const getData = async () => {
   //console.log(accounts[0]);
 
   const instance = new web3.eth.Contract(
-    JSON.parse(compileERC721Interface.interface),
+    JSON.parse(compileKittyCore.interface),
     '0x06012c8cf97BEaD5deAe237070F9587f8E7A266d'
   );
   const totalSupply = await instance.methods.totalSupply().call();
@@ -23,21 +23,25 @@ const getData = async () => {
   // const _owner = await instance.methods.ownerOf(1).call();
   // console.log(_owner);
 
-  for (var i = 1; i < 1000; i++) {
+  for (var i = 1; i < 100; i++) {
     const _owner = await instance.methods.ownerOf(i).call();
+    const _stats = await instance.methods.getKitty(i).call();
     console.log('on cat number ' + i + '/' + totalSupply);
     data.push({
       catID: i,
-      owner: _owner
-    });
-  }
-
-  for (var i = 1001; i < 2000; i++) {
-    const _owner = await instance.methods.ownerOf(i).call();
-    console.log('on cat number ' + i + '/' + totalSupply);
-    data.push({
-      catID: i,
-      owner: _owner
+      owner: _owner,
+      stats: {
+        isGestating: _stats.isGestating,
+        isReady: _stats.isReady,
+        cooldownIndex: _stats.cooldownIndex,
+        nextActionAt: _stats.nextActionAt,
+        siringWithId: _stats.siringWithId,
+        birthTime: _stats.birthTime,
+        matronId: _stats.matronId,
+        sireId: _stats.sireId,
+        generation: _stats.generation,
+        genes: _stats.genes
+      }
     });
   }
 
