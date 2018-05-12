6cadb306547e7a0ebac4d395220a8c4febfd3546
ycryptx <yonatan.med@gmail.com>
2018-05-12 16:15:32 -0400

    server backend v1

--- server/app.js
+++ server/app.js
@@ -2,10 +2,25 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/db.routes');
 
 var app = express();
+
+// Set native promises as mongoose promise
+mongoose.Promise = global.Promise;
+
+// MongoDB Connection
+mongoose.connect('mongodb://localhost/catnip', (error) => {
+  if (error) {
+    console.error('Please make sure Mongodb is installed and running!'); // eslint-disable-line no-console
+    throw error;
+  }
+  console.log("CONNECTING TO MONGOOSE");
+  // feed some dummy data in DB.
+  //dummyData();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -19,24 +34,9 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
-
-// Set native promises as mongoose promise
-mongoose.Promise = global.Promise;
-
-// MongoDB Connection
-mongoose.connect(serverConfig.mongoURL, (error) => {
-  if (error) {
-    console.error('Please make sure Mongodb is installed and running!'); // eslint-disable-line no-console
-    throw error;
-  }
-  console.log("CONNECTING TO MONGOOSE");
-  // feed some dummy data in DB.
-  //dummyData();
-});
-
 // error handler
 app.use(function(err, req, res, next) {
- console.log(err.message); 
+ console.log(err.message);
 });
 
 module.exports = app;
--- server/controllers/db.controller.js
+++ server/controllers/db.controller.js
@@ -1,6 +1,8 @@
-const Cat = require('../models/cryptokitties').Cat;
-const Answer = require('../models/cryptokitties').Web3;
+const Cat = require('../models/kitties.models').Cat;
+const Web3 = require('../models/kitties.models').Web3;
 const mongoose = require('mongoose');
+const scraper = require('../scraper');
+const mockID = 748523;
 
 /**
 FUNCTIONALITIES:
@@ -13,28 +15,102 @@ FUNCTIONALITIES:
 
 */
 
+/**
 
+*/
 function addCat(req, res) {
+  console.log('adding a kitty to the database!');
+
+  const data = {
+    name: req.body.name || null,
+    img: req.body.img || null,
+    _id: req.body.kittyid || null,
+    username: req.body.username || null,
+    owner: req.body.owner || null,
+    cattributes: req.body.cattributes || null,
+    parents: req.body.parents || null,
+    bio: req.body.bio || null,
+    birthtime: req.body.birthtime || null,
+    siringwith: req.body.siringwith || null,
+    cooldown: req.body.cooldown || null,
+    generation: req.body.generation || null
+  }
+
+  const newCat = new Cat(data);
+  newCat.save()
+  .then(cat => console.log(cat))
+  .catch((err) => {res.status(401).send({error: err})});
+}
+
+function scrapeKittyAndUpdate(req, res) {
+  console.log('updating kitty information from the scraper!');
+
+  const id = req.body.id;
+  const data = {
+    name: req.body.name || null,
+    img: req.body.img || null,
+    username: req.body.username || null,
+    cattributes: req.body.cattributes || null,
+    parents: req.body.parents || null,
+    bio: req.body.bio || null,
+    birthtime: req.body.birthtime || null
+  }
+
+  Cat.findByIdAndUpdate(id, data)
+  .then(cat => console.log(cat))
+  .catch((err) => {res.status(401).send({error: err})});
 
 }
 
-function updateCat(req, res) {
+function updateSiring(req, res) {
+  console.log('updating cat data!');
 
+  const id = req.body.kittyid;
+  const data = {
+    siringwith: req.body.siringwith || null
+  }
+
+  Cat.findByIdAndUpdate(id, data)
+  .then(cat => console.log(cat))
+  .catch((err) => {res.status(401).send({error: err})});
 }
 
-function siringWith(req, res {
 
+function addAddress(req, res) {
+  console.log('Adding an address!');
+
+
+  const data = {
+    _id: req.body.address,
+    balance: req.body.balance || null,
+    kittyid: req.body.kittyid || null
+  }
+
+  const newAddress = new Web3(data);
+  newAddress.save()
+  .then(address => console.log(address))
+  .catch((err) => {res.status(401).send({error: err})});
 }
+
 
 function updateBalance(req, res){
+  console.log('updating the balance of the user!');
 
-}
+  const address = req.body.address;
+  //can be negative / positive. Gets added to the current balance
+  const payout = parseInt(req.body.payout);
 
-function associateCat(req, res){
-  
+  Web3.findByIdAndUpdate(address, {$inc: {balance: payout}})
+  .then(user => console.log(user))
+  .catch((err) => {res.status(401).send({error: err})});
+
 }
 
 
 module.exports =  {
-
+  addCat: addCat,
+  scrapeKittyAndUpdate: scrapeKittyAndUpdate,
+  updateBalance: updateBalance,
+  updateSiring: updateSiring,
+  addAddress: addAddress
 }
