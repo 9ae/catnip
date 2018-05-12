f0b75ce66f57889d43a5350dc4b71654c11c534e
alice <alice@valour.me>
2018-05-12 15:33:23 -0400

    save for now

--- dapp/src/App.js
+++ dapp/src/App.js
@@ -7,13 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <div className="App-intro">
-          <SwipeCompo />
-        </div>
+        <SwipeCompo />
       </div>
     );
   }
--- dapp/src/SwipeCompo.js
+++ dapp/src/SwipeCompo.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Cards, { Card } from 'react-swipe-card';
+
 import './Swipe.css'
 
 class SwipeCompo extends Component {
@@ -7,12 +8,18 @@ class SwipeCompo extends Component {
     constructor(props) {
       super(props);
       this.state = {
-        cats : ['Garfield', 'Meowth', 'Hello Kitty']
+        cats : ['Garfield', 'Meowth', 'Hello Kitty'],
+        isReady: false
       };
     }
 
+    componentDidMount() {
+      this.setState({isReady: true})
+    }
+
     action (eventType) {
-      console.log(eventType);
+      console.log(this.state.isReady);
+      console.log(eventType +' on '+ this.state.cats[0]);
       /*
       if(this.state.cats.length > 0 ) {
         this.setState({ cats: this.state.cats.slice(0,1) })
