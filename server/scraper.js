4ad837970112b5c089f92b5f5c6eb61bf5f5158d
Marton Pono <marci93@gmail.com>
2018-05-12 16:26:03 -0400

    Getting kitty data

--- server/scraper.js
+++ server/scraper.js
@@ -1,9 +1,34 @@
-const request = require('request'); 
-const cheerio = require('cheerio'); 
+const request = require('request-promise-native'); 
+const cheerio = require('cheerio');
+const phantom = require('phantom');
+var fs = require('fs');
 
-const scrapeKitty = (kittyID) => {
+const scrapeKitty = async (kittyID) => {
 
-	return 'blabla'; 
+	const url = 'https://api.cryptokitties.co/kitties/' + kittyID; 
+
+	return request(url)
+	.then((res) => {
+		const jsonRes = JSON.parse(res); 
+		const cattributes = jsonRes.enhanced_cattributes
+			.filter((cattribute) => {
+				return true; 
+			}).map((cattribute) =>{
+				return cattribute.type + '|' + cattribute.description; 
+			});
+
+		return {
+			name: jsonRes.name ? jsonRes.name : 'Kitty #' + kittyID, 
+			img: jsonRes.image_url, 
+			username: jsonRes.owner.nickname,
+			cattributes: cattributes,
+			bio: jsonRes.bio,
+			birthtime: jsonRes.created_at
+		};  
+	}).catch((err) => {
+		console.log(err)
+	});
+
 
 }; 
 
