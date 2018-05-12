7eecf09085eabad2850a1b3d350c5e4b6c8b625f
Marton Pono <marci93@gmail.com>
2018-05-12 18:41:13 -0400

    Added Request Handler

---
+++ server/controllers/requestHandlers.js
@@ -0,0 +1,28 @@
+const dbController = require('./db.controller'); 
+
+const handleGetKittyList = (req, res) => {
+
+};
+
+const handleUpdateKittyListing = (req, res) => {
+
+
+};
+
+const handleGetKittiesToDisplay = (req, res) => {
+
+
+};
+
+const handleVoteOnKitty = (req, res) => {
+
+
+};
+
+
+module.exports = {
+	handleGetKittyList : handleGetKittyList, 
+	handleUpdateKittyListing: handleUpdateKittyListing,
+	handleGetKittiesToDisplay : handleGetKittiesToDisplay,
+	handleVoteOnKitty : handleVoteOnKitty
+}; 
