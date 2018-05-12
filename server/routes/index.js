2ba3dd2015c9a90e2e33ee375d6287a5b7824486
Marton Pono <marci93@gmail.com>
2018-05-12 13:58:19 -0400

    Added expres project

---
+++ server/routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+const scraper = require('../scraper'); 
+
+const mockID = 748523; 
+
+
+router.post('/registerKitties', (req, res) => {
+
+	console.log(scraper.scrapeKitty(mockID)); 
+
+})
---
+++ server/scraper.js
@@ -0,0 +1,12 @@
+const request = require('request'); 
+const cheerio = require('cheerio'); 
+
+const scrapeKitty = (kittyID) => {
+
+
+
+}; 
+
+return {
+	scrapeKitty: scrapeKitty
+}; 
