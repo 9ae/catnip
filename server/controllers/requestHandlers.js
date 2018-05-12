c309d7470666d698d8c497abf061385d0f76f3ca
Marton Pono <marci93@gmail.com>
2018-05-12 19:24:37 -0400

    started handle get kitty

--- server/controllers/requestHandlers.js
+++ server/controllers/requestHandlers.js
@@ -1,6 +1,26 @@
 const dbController = require('./db.controller'); 
+const request = require('request-promise-native'); 
+
+
 
 const handleGetKittyList = (req, res) => {
+
+	const address = req.query.address; 
+	const getKittiesURL = 'https://api.cryptokitties.co/kitties?owner_wallet_address='; 
+
+	request(getKittiesURL + address)
+		.then((r) => {
+			const res = JSON.parse(r); 
+			const kitties = res.kitties.map((kitty) => {
+				return {
+					name: kitty.name ? kitty.name : 'Kitty #' + kitty.id
+					id: kitty.id 
+				}; 
+			});
+
+			 
+
+		})
 
 };
 
